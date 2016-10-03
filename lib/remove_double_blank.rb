2b33b2e2fa91949d04fbeb58d05afa3222689253
Jason Hsu <jhsu802701@jasonhsu.coM>
2016-10-03 17:43:46 +0000

    Passes first meaningful test

--- lib/remove_double_blank.rb
+++ lib/remove_double_blank.rb
@@ -2,6 +2,22 @@ require 'remove_double_blank/version'
 
 #
 module RemoveDoubleBlank
-  # Your code goes here...
+  def self.update(path)
+    path_old = path
+    path_new = "#{path_old}.new"
+    file_w = open(path_new, 'w')
+    n_empty = 0
+    File.readlines(path_old).each do |line|
+      if line == "\n"
+        n_empty += 1
+      else
+        n_empty = 0
+      end  
+      file_w.write line if n_empty < 2
+    end
+    file_w.close
+    system("rm #{path_old}")
+    system("mv #{path_new} #{path_old}")
+  end
 end
 # Your new gem is a module by default.  You may wish to use a class instead.
--- spec/remove_double_blank_spec.rb
+++ spec/remove_double_blank_spec.rb
@@ -1,4 +1,10 @@
 require 'spec_helper'
+require 'remove_double_blank/version'
+
+# True if files are identical; false otherwise
+def filematch(file1, file2)
+  system("diff #{file1} #{file2} > /dev/null")
+end
 
 describe RemoveDoubleBlank do
   it 'has a version number' do
@@ -8,4 +14,23 @@ describe RemoveDoubleBlank do
   it 'does something useful' do
     expect(true).to eq(true)
   end
+
+  system('rm -rf tmp1')
+  system('cp -r test_files tmp1')
+  file1a = 'tmp1/test1a.txt'
+  file1b = 'tmp1/test1b.txt'
+  file2a = 'tmp1/test2a.txt'
+  file2b = 'tmp1/test2b.txt'
+  file3a = 'tmp1/test3a.txt'
+  file3b = 'tmp1/test3b.txt'
+  file4a = 'tmp1/test4a.txt'
+  file4b = 'tmp1/test4b.txt'
+  file5a = 'tmp1/test5a.txt'
+  file5b = 'tmp1/test5b.txt'
+
+  it 'handles test1b.txt' do
+    expect(filematch(file1a, file1b)).to eq(false)
+    RemoveDoubleBlank.update(file1a)
+    expect(filematch(file1a, file1b)).to eq(true)
+  end  
 end
